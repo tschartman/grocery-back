cf9cc893bb97fda807daa76ae7e73120bcfb5924
Tim Schartman <tschartman@yahoo.com>
2019-07-14 21:40:29 -0500

    allow posts/gets/deletes by id

--- visits/urls.py
+++ visits/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from visits import views
 
 urlpatterns = [
     path('visits/', views.visit_list),
     path('visits/<int:pk>/', views.visit_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
