0934a016b28c155c141e69d27d90a1b243a0e598
Tim Schartman <tschartman@yahoo.com>
2019-07-15 21:29:21 -0500

    add auth classes to restric usage of api

--- visits/models.py
+++ visits/models.py
@@ -8,6 +8,8 @@ class Visit(models.Model):
     items = models.IntegerField()
     total = models.FloatField()
     location = models.CharField(max_length=100, blank=False)
+    owner = models.ForeignKey('auth.User', related_name='visits', on_delete=models.CASCADE)
+
 
     class Meta:
         ordering = ('date',)
--- visits/views.py
+++ visits/views.py
@@ -1,18 +1,36 @@
 from visits.models import Visit
 from visits.serializers import VisitSerializer
 from rest_framework import generics
+from rest_framework import permissions
+from django.contrib.auth.models import User
+from visits.serializers import UserSerializer
+from visits.permissions import IsOwnerOrReadOnly
 
 
 class VisitList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
     """
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Visit.objects.all()
     serializer_class = VisitSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class VisitDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a grocery visit.
     """
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
     queryset = Visit.objects.all()
     serializer_class = VisitSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
