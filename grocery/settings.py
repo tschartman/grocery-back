be9f5f87f3635e99165702e8ef000fa26f316c2d
Tim Schartman <tschartman@yahoo.com>
2019-08-03 21:19:44 -0500

    added cors middleware

--- grocery/settings.py
+++ grocery/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'visits.apps.VisitsConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -105,6 +107,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
